f524cc4b09540df8045ab503ea2e46a60e9992da
asterfinde <dante.frias@gmail.com>
2017-04-28 12:31:54 -0500

    Add title functionality for project show page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,26 @@
+#	Helpers are methods you can define in the files in app/helpers, and they’re made
+#	available in your views. Helpers are for extracting the logic from the views; views should
+#	just be about displaying information. Every controller that comes from the controller
+#	generator has a corresponding helper, and another helper module exists for the entire
+#	application: the ApplicationHelper module, which lives at app/helpers/
+#	application_helper.rb
+
 module ApplicationHelper
+	# When you specify an argument in a method beginning with the splat operator (*), any
+	# arguments passed from this point will be available in the method as an array.
+	def title(*parts)
+
+		# If no arguments are passed to the title method, parts will be empty and empty? 
+		# will return true.
+		unless parts.empty?
+			
+			# If parts are specified for the title method, then you use the content_for method
+			# to define a named block of content, giving it the name title. Inside this content
+			# block, you join the parts together using a hyphen (-), meaning that this helper will
+			# output something like "Sublime Text 3 - Projects - Ticketee".
+			content_for :title do
+				(parts << "Ticketee").join(" - ")
+			end
+		end
+	end
 end
--- spec/features/creating_projects_spec.rb
+++ spec/features/creating_projects_spec.rb
@@ -8,5 +8,19 @@ RSpec.feature "Users can create new projects" do
 		fill_in "Description", with: "A text editor for everyone"
 		click_button "Create Project"
 		expect(page).to have_content "Project has been created."
+
+		# Uses the find_by method to find a project by its name. This finds
+		# the project that has just been created by the code directly above it
+		# on the first "fill_in" method
+		project = Project.find_by(name: "Sublime Text 3")
+
+		# Ensures that you’re on what should be the show action in the ProjectsController
+		expect(page.current_url).to eq project_url(project)
+
+		# These two last lines find the title element on the page by using Capybara’s
+		# find method and check using have_title that this element contains the page title of
+		# “Sublime Text 3 - Projects - Ticketee.”
+		title = "Sublime Text 3 - Projects - Ticketee"
+		expect(page).to have_title title
 	end
 end
